b26749e36d85a5adfc602f3f4d5682e06a4e6405
Caetano Melone <melone1@llnl.gov>
2025-05-21 11:44:30 -0700

    add initial bot features
    This PR allows a user to trigger these commands from within a PR of a
    Hubcast-enabled repo:
    
    - `/hubcast help`: see this message
    - `/hubcast approve`: sync this pull request with the destination repository and trigger a new pipeline
    - `/hubcast run pipeline`: request a new run of the GitLab CI pipeline for any reason
    
    ---
    
    These permissions are required for the Github app:
    
        - read: repo contents
        - read and write: pull requests (read pull request refs and write to pull request contents)
        - read and write: checks
        - read: issues (subscribe to issue/PR comments)
    
    I'm looking for feedback on code style and modularity. I think there's a
    lot of opportunity to consolidate shared logic but I'm not 100% sure of
    the way you'd like to go about it.
    
    closes #121

---
+++ src/hubcast/web/comments.py
@@ -0,0 +1,11 @@
+help_message = """
+You can interact with me in many ways!
+
+- `/hubcast help`: see this message
+- `/hubcast approve`: sync this pull request with the destination repository and trigger a new pipeline
+- `/hubcast run pipeline`: request a new run of the GitLab CI pipeline for any reason
+
+If you are an outside contributor to this repository, a maintainer will need to approve and run pipelines on your behalf.
+
+For assistance and bug reports, open an issue [here](https://github.com/llnl/hubcast/issues).
+"""
