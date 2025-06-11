bc1068c4e68817800a05ea831e6b962273abd0be
Caetano Melone <melone1@llnl.gov>
2025-06-11 15:54:06 -0700

    add clickable link on check run summary

--- src/hubcast/clients/github/client.py
+++ src/hubcast/clients/github/client.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 import aiohttp
 import yaml
 from gidgethub import aiohttp as gh_aiohttp
@@ -32,8 +34,18 @@ class GitHubClient:
     async def set_check_status(
         self, ref: str, check_name: str, status: str, details_url: str
     ):
+        gitlab_netloc = urlparse(details_url).netloc
+
         # construct upload payload
-        payload = {"name": check_name, "head_sha": ref, "details_url": details_url}
+        payload = {
+            "name": check_name,
+            "head_sha": ref,
+            "details_url": details_url,
+            "output": {
+                "title": "External Pipeline Run",
+                "summary": f"[View this pipeline on {gitlab_netloc}]({details_url})",
+            },
+        }
 
         # for success and failure status write out a conclusion
         if status in ("success", "failure", "cancelled"):
